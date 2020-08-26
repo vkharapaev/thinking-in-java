cd773f9095850bb9b4738790ad16fd173c2d6af0
Vladimir Kharapaev <vkharapaev@gmail.com>
2020-08-27 02:55:21 +0500

    2

--- io/hw/Ex12.java
+++ io/hw/Ex12.java
@@ -6,6 +6,6 @@ public class Ex12 {
     public static void main(String[] args) throws IOException {
 
         PrintWriter io = new PrintWriter(new BufferedWriter(new FileWriter("Ex12.txt")));
-//        io.pri
+
     }
 }
