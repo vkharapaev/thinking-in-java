729fd9ccb545ab68b42f7d205337d611d686be3f
vkharapaev <vkharapaev@gmail.com>
2020-08-26 11:37:21 +0500

    books

---
+++ io/hw/Ex12.java
@@ -0,0 +1,11 @@
+package io.hw;
+
+import java.io.*;
+
+public class Ex12 {
+    public static void main(String[] args) throws IOException {
+
+        PrintWriter io = new PrintWriter(new BufferedWriter(new FileWriter("Ex12.txt")));
+        io.pri
+    }
+}
