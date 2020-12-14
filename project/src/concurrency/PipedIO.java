028c12a2d29c43a6bd390c80e5797c9863786e43
Vladimir Kharapaev <vkharapaev@gmail.com>
2020-12-14 13:52:27 +0500

    cuncurrency

---
+++ project/src/concurrency/PipedIO.java
@@ -0,0 +1,64 @@
+package concurrency;
+
+import java.io.IOException;
+import java.io.PipedReader;
+import java.io.PipedWriter;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class Sender implements Runnable {
+    private Random rand = new Random(47);
+    private PipedWriter out = new PipedWriter();
+
+    public PipedWriter getPipedWriter() {
+        return out;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                for (char c = 'A'; c <= 'z'; c++) {
+                    out.write(c);
+                    Thread.sleep(rand.nextInt(500));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e + " Sender writer exception");
+        } catch (InterruptedException e) {
+            System.out.println(e + "Sender sleep interrupted");
+        }
+    }
+}
+
+class Receiver implements Runnable {
+    private PipedReader in;
+
+    public Receiver(Sender sender) throws IOException {
+        in = new PipedReader(sender.getPipedWriter());
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                System.out.print("Read: " + (char) in.read() + ", ");
+            }
+        } catch (IOException e) {
+            System.out.println(e + "Receiver read exception");
+        }
+    }
+}
+
+public class PipedIO {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Sender sender = new Sender();
+        Receiver receiver = new Receiver(sender);
+        ExecutorService exec = Executors.newCachedThreadPool();
+        exec.execute(sender);
+        exec.execute(receiver);
+        Thread.sleep(4000);
+        exec.shutdownNow();
+    }
+}
